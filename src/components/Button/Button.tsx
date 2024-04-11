20adc6806179beb5792beac4b52cdfa384b321ab
skoanton <antonhakanssonn@gmail.com>
2024-04-11 12:59:18 +0200

    la till några andra ljudeffekter och 2 mer kategorier

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,8 +4,8 @@ import "./Button.scss"
 import { useNavigate } from "react-router-dom"
 import { ACTION } from "../ProviderContext/Provider";
 import useSound from "use-sound";
-import hoverSound from "../../assets/tap-notification-180637.mp3"
-import clickSound from "../../assets/interface-124464.mp3";
+import hoverSound from "../../assets/menu-selection-102220.mp3"
+import clickSound from "../../assets/click-button-app-147358.mp3";
 type ButtonProps = {
     menu?: boolean;
     text?: string;
@@ -21,6 +21,7 @@ const Button = ({ menu, text, smallCircle, svg, onHandleClick, navigate, page, r
     const nav = useNavigate();
     const { dispatch } = useContext(WordContext);
     const [playClick] = useSound(clickSound);
+    const [playHover] = useSound(hoverSound);
     const handleOnClick = () => {
 
         playClick();
@@ -33,16 +34,20 @@ const Button = ({ menu, text, smallCircle, svg, onHandleClick, navigate, page, r
 
     }
 
+    const handleMouseEnter = () => {
+        playHover();
+    }
+
     return (
         <>
             {navigate && randomizeWord && text ? (
-                <button onClick={() => { handleOnClick(); handelNavigate(); dispatch({ type: ACTION.RANDOMIZEWORD, payload: text }) }} className={`button ${menu ? "buttonMenu" : smallCircle ? "smallCircle" : "bigCircle"}`}>{svg} {text?.toUpperCase()}</button>
+                <button onMouseEnter={handleMouseEnter} onClick={() => { handleOnClick(); handelNavigate(); dispatch({ type: ACTION.RANDOMIZEWORD, payload: text }) }} className={`button ${menu ? "buttonMenu" : smallCircle ? "smallCircle" : "bigCircle"}`}>{svg} {text?.toUpperCase()}</button>
 
             ) : navigate ?
                 (
-                    <button onClick={() => { handleOnClick(); handelNavigate(); }} className={`button ${menu ? "buttonMenu" : smallCircle ? "smallCircle" : "bigCircle"}`}>{svg} {text?.toUpperCase()}</button>
+                    <button onMouseEnter={handleMouseEnter} onClick={() => { handleOnClick(); handelNavigate(); }} className={`button ${menu ? "buttonMenu" : smallCircle ? "smallCircle" : "bigCircle"}`}>{svg} {text?.toUpperCase()}</button>
                 ) :
-                <button onClick={() => { if (onHandleClick) onHandleClick(); handleOnClick(); }} className={`button ${menu ? "buttonMenu" : smallCircle ? "smallCircle" : "bigCircle"}`}>{svg} {text?.toUpperCase()}</button>
+                <button onMouseEnter={handleMouseEnter} onClick={() => { if (onHandleClick) onHandleClick(); handleOnClick(); }} className={`button ${menu ? "buttonMenu" : smallCircle ? "smallCircle" : "bigCircle"}`}>{svg} {text?.toUpperCase()}</button>
 
             }
         </>
